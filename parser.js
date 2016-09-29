96d87a8b34e7c7e64aff6042506295b027e80cd9
per4uk <per4uk@per4uks-MacBook-Pro.local>
2016-09-29 13:49:13 +0300

    Parser ctx data

---
+++ parser.js
@@ -0,0 +1,74 @@
+var fs = require('fs'),
+    xml2js = require('xml2js'),
+    util = require('util');
+
+
+var parseNumbers = function ( str ) {
+
+    if ( !isNaN( str ) ) {
+
+      str = str % 1 === 0 ? parseInt( str, 10 ) : parseFloat( str );
+
+    }
+
+    return str;
+
+};
+
+var firstCharLowerCase = function ( str ) {
+
+    return str.charAt( 0 ).toLowerCase() + str.slice( 1 );
+
+};
+
+var parser = new xml2js.Parser({
+    explicitArray : false,
+    valueProcessors: [parseNumbers],
+    attrValueProcessors: [parseNumbers],
+    attrNameProcessors: [firstCharLowerCase],
+    tagNameProcessors: [firstCharLowerCase]
+}); 
+
+var convertFileTCXtoJSON = function ( inputFile, outputFile, callback ) {
+
+    fs.readFile( inputFile, function( err, data ) {
+
+        if ( err ) return callback( err );
+
+        parser.parseString( data, function ( err, result ) {
+
+            if ( err ) return callback( err );
+
+            fs.writeFile( outputFile, JSON.stringify( result, null, 4 ), function ( err ) {
+
+                if ( err ) return callback( err );
+                callback( null, 'Done' );
+
+            });
+
+        });
+
+    });
+
+};
+
+var convertDataTCXtoJSON = function ( inputData, callback ) {
+
+    parser.parseString( inputData, function ( err, result ) {
+
+        return callback( err, result );
+
+    });
+
+};
+
+convertFileTCXtoJSON( __dirname + '/20160911_135114.tcx', __dirname + '/1.js', function ( err ) {
+
+    console.log( err );
+
+});
+
+module.exports = {
+    convertDataTCXtoJSON: convertDataTCXtoJSON,
+    convertFileTCXtoJSON: convertFileTCXtoJSON
+}
